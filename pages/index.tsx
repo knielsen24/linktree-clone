7c97300998a9efc89bf7f171b19412471bac9088
Kevin Nielsen <knielsen24@gmail.com>
2023-02-16 15:01:29 -0800

    disabled eslint rules

--- pages/index.tsx
+++ pages/index.tsx
@@ -140,6 +140,7 @@ export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center mx-auto w-full mt-16 px-8 ">
             <Image
+                priority
                 className="rounded-full"
                 alt={data.name}
                 src={data.avatar}
