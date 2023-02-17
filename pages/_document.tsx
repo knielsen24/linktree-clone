4a584a1e070257c3354f3df5382f29e72cd4618e
Kevin Nielsen <knielsen24@gmail.com>
2023-02-16 22:00:08 -0800

    added meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,11 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head />
+            <title>Kevin Nielsen Linktree Clone Next.js</title>
+            <meta
+                name="description"
+                content="This is a Linktree Clone.  The project was built by Kevin Nielsen in next.js using tailwindcss."
+            />
             <body className="bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-indigo-200 via-slate-600 to-indigo-200">
                 <Main />
                 <NextScript />
